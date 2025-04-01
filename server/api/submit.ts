c68a04825c2d191612e2c255742a06770fe1f8fd
charlesx19 <charlesx19@gmail.com>
2025-04-01 11:41:37 +0800

    feat: refactor submit feature

--- server/api/submit.ts
+++ server/api/submit.ts
@@ -11,7 +11,7 @@ export default defineEventHandler(async (event) => {
 
   try {
     const response = await fetch(config.gasUrl, {
-      method: "POST",
+      method: "GET",
       body: JSON.stringify(body),
       headers: {
         "Content-Type": "application/json",
